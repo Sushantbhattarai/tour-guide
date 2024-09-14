4237327c3b330ae1f8d210c2191e37b6c9a61b74
Sushantbhattarai <146710731+Sushantbhattarai@users.noreply.github.com>
2024-09-14 21:30:02 +0545

    Api Service

---
+++ app/src/main/java/com/example/nit3213/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.nit3213
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ApiService {
+    @POST("/ort/auth")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
+
+    @GET("/dashboard/{keypass}")
+    suspend fun getDashboard(@Path("keypass") keypass: String): Response<DashboardResponse>
+}
